ad44a72407ae4681f6b45ff2febe9de00420407d
unknown <tritechuzaifa@gmail.com>
2023-01-26 10:58:33 +0500

    ALi

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Helpers\Api;
+use App\Helpers\ApiValidate;
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        $credentials = ApiValidate::login($request, Rider::class);
+
+        if (Auth::guard('user')->attempt($credentials)) {
+            $rider = User::find(Auth::guard('rider')->user()->id);
+            $rider->account;
+            return Api::setResponse('rider', $rider->withToken());
+        } else {
+            return Api::setError('Invalid credentials');
+        }
+    }
+
+    public function register(Request $request){
+
+
+        User::create(['password' => Hash::make($request->password)]+$request->all());
+        return redirect()->back();
+    }
+}
