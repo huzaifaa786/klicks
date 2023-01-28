3b8333338a313d1f1f34fe8f5ed1ef2365888610
unknown <tritechuzaifa@gmail.com>
2023-01-28 14:53:28 +0500

    ALi

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
-class admincontroller extends Controller
+class AdminController extends Controller
 {
     public function login(Request $request)
     {
