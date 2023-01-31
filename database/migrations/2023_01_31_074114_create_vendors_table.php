00dbc0492622792938fba412fdc2cfa81dc28622
unknown <tritechuzaifa@gmail.com>
2023-01-31 13:34:33 +0500

    ALi

--- database/migrations/2023_01_31_074114_create_vendors_table.php
+++ database/migrations/2023_01_31_074114_create_vendors_table.php
@@ -19,7 +19,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('phone');
-            $table->string('api_token');
+       
 
             $table->string('password');
             $table->timestamps();
