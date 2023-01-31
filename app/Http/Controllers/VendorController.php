553fddea2a9cb3a300a06c612cc8db3158d6a9bc
unknown <tritechuzaifa@gmail.com>
2023-01-31 14:01:55 +0500

    ALi

--- app/Http/Controllers/VendorController.php
+++ app/Http/Controllers/VendorController.php
@@ -4,12 +4,13 @@ namespace App\Http\Controllers;
 
 use App\Models\Vendor;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class VendorController extends Controller
 {
     public function store(Request $request)
     {
-        Vendor::create($request->all());
+        Vendor::create(['password' => Hash::make($request->password)]+$request->all());
         return redirect()->back();
     }
 }
