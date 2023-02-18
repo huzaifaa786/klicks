4ccafb7183faa750b26fd588e5fc7c956a40602d
unknown <tritechuzaifa@gmail.com>
2023-02-18 12:21:44 +0500

    ALi

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -66,7 +66,7 @@ class AuthController extends Controller
 
 
         // toastr()->success('update successfully ');
-        return Api::setResponse('comapny', $data);
+        return Api::setResponse('company', $data);
     }
 
 }
